1346551ec8aa784eb472c536ba2cf561900ad79d
Ben Smith <binji@chromium.org>
2019-08-28 15:26:32 -0400

    Add a toolbar

--- asm.js
+++ asm.js
@@ -1,33 +1,55 @@
-/// UI stuff
 const $ = document.querySelector.bind(document);
-const editor = ace.edit('input');
 
+const initialProgram =
+`int fac(int n) {
+    if (n < 1) return 1;
+    return n * fac(n - 1);
+}
+`;
+
+// Warn on close. It's easy to accidentally hit Ctrl+W.
+window.addEventListener('beforeunload', event => {
+  event.preventDefault();
+  event.returnValue = '';
+});
+
+const compile = debounceLazy(async () => {
+  const input = `test.cc`;
+  const contents = editor.getValue();
+  await api.compileToAssembly(input, contents);
+}, 100);
+
+const setKeyboard = name => editor.setKeyboardHandler(`ace/keyboard/${name}`);
+
+// Toolbar stuff
+$('#vim').addEventListener('click', event => setKeyboard('vim'));
+$('#emacs').addEventListener('click', event => setKeyboard('emacs'));
+$('#sublime').addEventListener('click', event => setKeyboard('sublime'));
+
+// Editor stuff
+const editor = ace.edit('input');
 editor.session.setMode('ace/mode/c_cpp');
 editor.setKeyboardHandler('ace/keyboard/vim');
 editor.setOption('fontSize', 20);
-
-editor.session.on('change', debounceLazy(compileToAssembly, 100));
-
-editor.setValue(`int fac(int n) {
-    if (n < 1) return 1;
-    return n * fac(n - 1);
-}`);
+editor.session.on('change', compile);
+editor.setValue(initialProgram);
 editor.clearSelection();
 
+// Terminal stuff
 Terminal.applyAddon(fit);
-
 const term = new Terminal({convertEol: true, disableStdin: true, fontSize: 20});
 term.open($('#output'));
 term.fit();
 
 Split({
-  rowGutters: [{track: 1, element: $('.gutter')}],
+  rowGutters: [{track: 2, element: $('.gutter')}],
   onDrag: () => {
     term.fit();
     editor.resize();
   }
 });
 
+// Compile stuff
 const api = new API({
   async readBuffer(filename) {
     const response = await fetch(filename);
@@ -37,10 +59,4 @@ const api = new API({
   hostWrite(s) { term.write(s); }
 });
 
-async function compileToAssembly() {
-  const input = `test.cc`;
-  const contents = editor.getValue();
-  await api.compileToAssembly(input, contents);
-}
-
-compileToAssembly();
+compile();
--- web.js
+++ web.js
@@ -1,19 +1,8 @@
-/// UI stuff
 const $ = document.querySelector.bind(document);
 
-const editor = ace.edit('input');
+const initialProgram =
+`#include <stdio.h>
 
-editor.session.setMode('ace/mode/c_cpp');
-editor.setKeyboardHandler('ace/keyboard/vim');
-editor.setOption('fontSize', 20);
-
-editor.commands.addCommand({
-  name: 'run',
-  bindKey: {win: 'Ctrl+Enter', mac: 'Command+Enter'},
-  exec: debounceLazy(editor => api.compileLinkRun(editor.getValue()), 100)
-});
-
-editor.setValue(`#include <stdio.h>
 int fac(int n) {
     if (n < 1) return 1;
     return n * fac(n - 1);
@@ -23,23 +12,51 @@ int main() {
     for (int i = 1; i <= 10; ++i) {
         printf("fac(%d) = %d\\n", i, fac(i));
     }
-}`);
+}
+`;
+
+// Warn on close. It's easy to accidentally hit Ctrl+W.
+window.addEventListener('beforeunload', event => {
+  event.preventDefault();
+  event.returnValue = '';
+});
+
+const run = debounceLazy(editor => api.compileLinkRun(editor.getValue()), 100);
+const setKeyboard = name => editor.setKeyboardHandler(`ace/keyboard/${name}`);
+
+// Toolbar stuff
+$('#run').addEventListener('click', event => run(editor));
+$('#vim').addEventListener('click', event => setKeyboard('vim'));
+$('#emacs').addEventListener('click', event => setKeyboard('emacs'));
+$('#sublime').addEventListener('click', event => setKeyboard('sublime'));
+
+// Editor stuff
+const editor = ace.edit('input');
+editor.session.setMode('ace/mode/c_cpp');
+editor.setKeyboardHandler('ace/keyboard/vim');
+editor.setOption('fontSize', 20);
+editor.commands.addCommand({
+  name: 'run',
+  bindKey: {win: 'Ctrl+Enter', mac: 'Command+Enter'},
+  exec: run
+});
+editor.setValue(initialProgram);
 editor.clearSelection();
 
-
+// Terminal stuff
 Terminal.applyAddon(fit);
 const term = new Terminal({convertEol: true, disableStdin: true, fontSize: 20});
 term.open($('#output'));
 term.fit();
 
+// Splitter
 Split({
-  rowGutters: [{track: 1, element: $('.gutter')}],
+  rowGutters: [{track: 2, element: $('.gutter')}],
   onDrag: debounceLazy(() => {
     term.fit();
     editor.resize();
   }, 10)
 });
-
 
 /// Compile stuff
 const api = new API({
