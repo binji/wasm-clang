1093b3ce0ece86eacfab94eacf94cc423ec0c44c
Ben Smith <binjimin@gmail.com>
2019-09-18 07:45:56 -0700

    more stuff

---
+++ examples/07_starter.cc
@@ -0,0 +1,22 @@
+#include <canvas.h>
+#include <stdint.h>
+
+const int w = 1000;
+const int h = 800;
+Canvas c{w, h};
+ImageData image{w, h};
+
+int main() {
+    for (int y = 0; y < h; ++y) {
+        for (int x = 0; x < w; ++x) {
+            image.data[y * w + x] = RGB(x | y, 0, 0);
+        }
+    }
+    image.commit();
+    c.putImageData(image, 0, 0);
+
+    const char* msg = "x | y";
+    c.setFillStyle("white");
+    c.setFont("bold 200px sans");
+    c.fillText(msg, (w - c.measureText(msg)) / 2, (h + 100) / 2);
+}
