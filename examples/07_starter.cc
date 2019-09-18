d80403edc0bf094ea4f4d0785018dbe77f3c8fb4
Ben Smith <binjimin@gmail.com>
2019-09-18 08:12:12 -0700

    Use canvas_main for starter

--- examples/07_starter.cc
+++ examples/07_starter.cc
@@ -1,22 +1,24 @@
-#include <canvas.h>
-#include <stdint.h>
+#include <canvas_main.h>
 
 const int w = 1000;
 const int h = 800;
 Canvas c{w, h};
 ImageData image{w, h};
 
-int main() {
-    for (int y = 0; y < h; ++y) {
-        for (int x = 0; x < w; ++x) {
-            image.data[y * w + x] = RGB(x | y, 0, 0);
-        }
+void setup() {
+  for (int y = 0; y < h; ++y) {
+    for (int x = 0; x < w; ++x) {
+      image.data[y * w + x] = RGB(x | y, 0, 0);
     }
-    image.commit();
-    c.putImageData(image, 0, 0);
+  }
+  image.commit();
+  c.putImageData(image, 0, 0);
 
-    const char* msg = "x | y";
-    c.setFillStyle("white");
-    c.setFont("bold 200px sans");
-    c.fillText(msg, (w - c.measureText(msg)) / 2, (h + 100) / 2);
+  const char *msg = "x | y";
+  c.setFillStyle("white");
+  c.setFont("bold 200px sans");
+  c.fillText(msg, (w - c.measureText(msg)) / 2, (h + 100) / 2);
+}
+
+void loop(double now, double elapsed) {
 }
